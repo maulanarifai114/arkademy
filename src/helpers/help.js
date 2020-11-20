76c93e59a684e7bb01da8b8aa1ba30585e8abc2c
Your Name <you@example.com>
2020-11-20 15:46:39 +0700

    add file help.js

---
+++ src/helpers/help.js
@@ -0,0 +1,12 @@
+module.exports = {
+    response: (res, result, status, err) => {
+        const resultPrint = {}
+        resultPrint.status = 'success'
+        resultPrint.statusCode = status
+        resultPrint.result = result
+        resultPrint.err = err || null
+        res.status(status)
+        res.json(resultPrint)
+    },
+
+}
