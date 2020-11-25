6c12618af898c82d4292f9d26d5dd1415b106140
maulanarifai114 <maulanarifai114@gmail.com>
2020-11-25 08:56:07 +0700

    solved with eslint

--- zwallet/src/router/index.js
+++ zwallet/src/router/index.js
@@ -1,23 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Login from '../views/auth/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [{
-  path: '/home',
-  name: 'Home',
-  component: Home
-},
-{
-  path: '/about',
-  name: 'About',
-  component: About
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  path: '/',
+  name: 'Login',
+  component: Login
 }
 ]
 
