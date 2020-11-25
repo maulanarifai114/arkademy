63c72cf6852a998fbda796219dea4cd09b579493
maulanarifai114 <maulanarifai114@gmail.com>
2020-11-26 01:12:30 +0700

    add class inibutton with bg red

--- zwallet/src/views/landing-page/Landing.vue
+++ zwallet/src/views/landing-page/Landing.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="landing">
         <router-link to="/login">
-            <button class="btn btn-primary">Ini Login</button>
+            <button class="btn btn-primary inibutton">Ini Login</button>
         </router-link>
         <router-link to="/signup">
             <button class="btn btn-primary">Ini SignUp</button>
@@ -11,10 +11,13 @@
 
 <script>
 export default {
-
+  name: 'Landing'
 }
 </script>
 
 <style scoped>
-
+.inibutton {
+    border-radius: 20px !important;
+    background-color: rgb(255, 133, 133);
+}
 </style>
