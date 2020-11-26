6c7fd6b79341e5258e9177025d6374489afcbae2
maulanarifai114 <maulanarifai114@gmail.com>
2020-11-26 15:54:23 +0700

    add auth and auth-views

---
+++ zwallet/src/components/auth/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>This is MainAuth</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MainAuth'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
