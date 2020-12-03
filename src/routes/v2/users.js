b6641846783e8e3ca901364c0dbd46495d0c4697
maulanarifai114 <maulanarifai114@gmail.com>
2020-12-03 13:57:37 +0700

    change routers in users

--- src/routes/v2/users.js
+++ src/routes/v2/users.js
@@ -2,14 +2,20 @@ const express = require('express')
 const router = express.Router()
 const usersControl = require('../../controllers/v2/users')
 const usersLog = require('../../controllers/v2/auth')
-const middleware = require('../../middleware/v2/auth')
+const {
+  verifyAccess
+} = require('../../middleware/v2/auth')
+const {
+  uploadMulter
+} = require('../../middleware/v2/upload')
 
 router
   .post('/register', usersLog.signUpUser)
   .post('/login', usersLog.loginUser)
-  .get('/', middleware.verifyAccess, usersControl.getAllUsers)
-  .put('/:id', usersControl.updateUser)
-  .delete('/:id', usersControl.deleteUser)
-  .delete('/phone/:id', usersControl.deletePhone)
+  .get('/', verifyAccess, usersControl.getAllUsers)
+  .post('/', uploadMulter.single('avatar'), usersControl.insertUser)
+  .put('/:id', verifyAccess, usersControl.updateUser)
+  .delete('/:id', verifyAccess, usersControl.deleteUser)
+  .delete('/phone/:id', verifyAccess, usersControl.deletePhone)
 // .post('/', usersControl.insertUser)
 module.exports = router
