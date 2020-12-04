276ca2c48126a6b663203c34c3a16c9efbd16887
maulanarifai114 <maulanarifai114@gmail.com>
2020-12-04 11:00:03 +0700

    making validation v2

--- src/middleware/v2/validation.js
+++ src/middleware/v2/validation.js
@@ -1,39 +1,21 @@
 const fs = require('fs')
-
+const helper = require('../../helpers/help')
 const valid = (req, res, next) => {
-  if (typeof (req.file) === 'undefined' || typeof (req.body) === 'undefined') {
-    return res.status(400).json({
-      errors: 'problem with sending data'
-    })
-  }
-  console.log(req.body.name)
-  let name = req.body.name
-  let image = req.file.path
-
-  console.log(req.file)
 
   if (!(req.file.mimetype).includes('jpeg') && !(req.file.mimetype).includes('png') && !(req.file.mimetype).includes('jpg')) {
     fs.unlinkSync(`./upload/${req.file.filename}`)
-    return res.status(400).json({
-      errors: "file not support"
+    return helper.reject(res, null, 400, {
+      error: 'File not supported'
     })
   }
 
   if (req.file.size > 1024 * 1024) {
     fs.unlinkSync(`./upload/${req.file.filename}`)
-    return res.status(400).json({
-      errors: "File is Too large"
+    return helper.reject(res, null, 400, {
+      error: 'File is too big'
     })
   }
   console.log(req.file)
-
-  if (!name || !image) {
-
-    return res.status(400).json({
-      sucess: false,
-      message: "all fields are required"
-    })
-  }
 
   next()
 }
