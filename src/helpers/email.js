65fb99d5b7a4024d4eb0c0ea129c1293d21b3cfa
maulanarifai114 <maulanarifai114@gmail.com>
2020-12-05 12:31:50 +0700

    made sendMail Work

---
+++ src/helpers/email.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer')
+
+exports.sendEmail = (email, text) => {
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: `${process.env.EMAIL_USERNAME}`, // generated ethereal user
+      pass: `${process.env.EMAIL_PASSWORD}`, // generated ethereal password
+    },
+  });
+
+  console.log('ini text = ', text)
+  return new Promise((resolve, reject) => {
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME, // sender address
+      to: email, // list of receivers
+      subject: "Email Activation", // Subject line
+      html: `
+      <p>
+        <a href="">Here </a>is your link activation
+        <p>${text}</p>
+      </p>`, // html body
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        resolve(info)
+      }
+    });
+  })
+}
